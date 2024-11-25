0876b3eabd67914e1af16071dadd25edc05c204e
chaggle <chaggle@foxmail.com>
2024-11-25 09:01:47 +0800

    init: 初始化

---
+++ src/main/java/cn/gb/gb28181/Gb28181Application.java
@@ -0,0 +1,13 @@
+package cn.gb.gb28181;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gb28181Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gb28181Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gb/gb28181/Gb28181ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gb.gb28181;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gb28181ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
